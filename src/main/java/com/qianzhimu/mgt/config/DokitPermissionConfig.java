21757e337ca57ab66390f6d176c79d9fad891ba1
xiangchi.fan <xiangchi.fan@hotmail.com>
2022-11-29 13:06:32 +0800

    to set

--- src/main/java/com/qianzhimu/mgt/config/DokitPermissionConfig.java
+++ src/main/java/com/qianzhimu/mgt/config/DokitPermissionConfig.java
@@ -4,14 +4,14 @@ import com.qianzhimu.api.utils.SecurityUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service(value = "dokit")
 public class DokitPermissionConfig {
 
     public Boolean check(String ...permissions){
-        List<String> dokitPermissions = SecurityUtils.getCurrentUser().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        Set<String> dokitPermissions = SecurityUtils.getCurrentUser().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
         return dokitPermissions.contains("admin") || Arrays.stream(permissions).anyMatch(dokitPermissions::contains);
     }
 
